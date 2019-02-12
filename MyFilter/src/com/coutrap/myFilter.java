a267e237dd8c8d063464987e09390a4130cc3f80
Alex logo <alexlogo@hevra.haifa.ac.il>
2019-02-12 20:22:58 +0200

    Deployment descriptor

--- MyFilter/src/com/coutrap/myFilter.java
+++ MyFilter/src/com/coutrap/myFilter.java
@@ -1,6 +1,7 @@
 package com.coutrap;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -8,7 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.HttpConstraintElement;
 /**
  * Servlet Filter implementation class myFilter
  */
@@ -33,9 +37,11 @@ public class myFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
+        HttpSession session = ((HttpServletRequest) request).getSession(false);
+        
+        if(session == null){
+        	((HttpServletResponse) response).sendRedirect("login.html");
+              }
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
 	}
--- MyFilter/src/com/coutrap/servlet.java
+++ MyFilter/src/com/coutrap/servlet.java
@@ -1,26 +1,85 @@
 package com.coutrap;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import enumerators.ClientType;
+import facade.CouponClientFacade;
+import system.CouponSystem;
 
 /**
  * Servlet implementation class servlet
  */
-@WebServlet("/servlet")
+@WebServlet("/LoginServlet")
 public class servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
+     * 
      */
+	private CouponSystem sys;
+	
+	public void init() {
+		this.sys = CouponSystem.getInstance();
+	}
+	
     public servlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
+    
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	HttpSession session = request.getSession(false);
+    	if (session != null) {
+    		session.invalidate(); //Kill the session
+    		session = request.getSession(true);
+    		
+    	} else {
+    		session = request.getSession(true); //Create new session
+    	}
+    	
+    	String user = request.getParameter("user");
+    	String type = request.getParameter("type");
+    	String password = request.getParameter("password");
+    	
+    	ClientType Ctype = ClientType.valueOf(type);
+    	
+    	try {
+			CouponClientFacade facade  = sys.login(user, password, Ctype);
+			if (facade != null) {
+				session.setAttribute("clientFacade", facade);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	switch (type) {
+		case "Customer":
+			request.getRequestDispatcher("customer.html").forward(request, response);
+			break;
+		case "Company":
+			request.getRequestDispatcher("company.html").forward(request, response);
+			break;
+		case "Admin":
+			request.getRequestDispatcher("admin.html").forward(request, response);
+			break;
+		default:
+			
+			break;
+			
+	
+		}
+    } 
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
