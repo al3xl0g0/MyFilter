732c7cc6dd1a1cc581e5cb6a5d36e59ca1f3716f
Alex logo <alexlogo@hevra.haifa.ac.il>
2019-02-12 20:43:54 +0200

    CouponClientFacade facade

--- MyFilter/src/com/coutrap/services/AdminService.java
+++ MyFilter/src/com/coutrap/services/AdminService.java
@@ -1,5 +1,21 @@
 package com.coutrap.services;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import facade.CouponClientFacade;
 
+@Path("/admin")
 public class AdminService {
+	
+	@Context
+	private HttpServletRequest request;
+	@Context
+	private HttpServletRequest response;
+	
+	public AdminService() {
+		
+		CouponClientFacade facade = (CouponClientFacade)this.request.getSession(false).getAttribute("CouponClientFacade");
+		
+	}
 
 }
